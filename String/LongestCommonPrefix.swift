38c4d3cb8828fa106ea0a507f4ee19447129c6f8
moon <cherishloveyou@hotmail.com>
2021-03-19 11:23:25 +0800

    longestCommonPrefix

--- String/LongestCommonPrefix.swift
+++ String/LongestCommonPrefix.swift
@@ -6,29 +6,13 @@
 
 class LongestCommonPrefix {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        guard let firstStr = strs.first else {
-            return ""
-        }
-        
-        var res = ""
-        
-        for (i, char) in firstStr.enumerated() {
-            // dropFirst(_ k: Int = 1) returns a Substring struct
-            for str in strs.dropFirst() {
-                if i == str.count {
-                    return res
-                }
-                
-                // Another easy way: Array(str)[i], time complexity is linear though
-                let currentStrChar = str[str.index(str.startIndex, offsetBy: i)]
-                
-                if char != currentStrChar {
-                    return res
-                }
+        guard var res = strs.first else { return "" }
+
+        for string in strs {
+            while !string.hasPrefix(res) {
+                res = String(res.dropLast())
             }
-            res.append(char)
         }
-        
         return res
     }
 }
